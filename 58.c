cbb89e6ac63efa4a7c6f2738ba21b1d5cec91ea8
AKHILASANTHOSH <30549500+AKHILASANTHOSH@users.noreply.github.com>
2018-02-07 16:20:06 +0530

    58.c

---
+++ 58.c
@@ -0,0 +1,14 @@
+include<stdio.h>
+#include<conio.h>
+int main()
+{
+int a,b;
+printf("enter the numbers:\n");
+scanf("%d%d",&a,&b);
+a=a^b;
+b=a^b;
+a=a^b;
+
+printf("swap numbers :%d%d",a,b);
+return 0;
+}
