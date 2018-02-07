e82b2b3276e27ae37050e529c1def16a058475b3
AKHILASANTHOSH <30549500+AKHILASANTHOSH@users.noreply.github.com>
2018-02-07 16:47:27 +0530

    68.c

---
+++ 68.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int n;
+printf("enter the number");
+scanf("%d",&n);
+if(n%7==0)
+{
+printf("divisible by 7");
+}
+else
+{
+printf("not divisible");
+}
+return 0;
+}
