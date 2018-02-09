482de1743a05bf7428b2750ec28a9ab900c91ca2
AKHILASANTHOSH <30549500+AKHILASANTHOSH@users.noreply.github.com>
2018-02-09 16:08:22 +0530

    1.c

---
+++ 1.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n;
+clrscr();
+printf("enter the number");
+scanf("%d",&n);
+if(n>0)
+{
+printf("the number is positive");
+}
+else
+{
+printf("the number is negative");
+}
+getch();
+}
