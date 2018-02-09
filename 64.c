72ae31d2b8d5f977aba0b7f7bb928f324a28e22e
AKHILASANTHOSH <30549500+AKHILASANTHOSH@users.noreply.github.com>
2018-02-09 15:53:51 +0530

    64.c

---
+++ 64.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() 
+{
+	int a,b,c;
+	printf("enter the two number:\n");
+	scanf("%d%d",&a,&b);
+	c=a+b;
+	if(c%2==0)
+	{
+		printf("even");
+		
+	}
+	else
+	{
+		printf("odd");
+	}
+	return 0;
+}
