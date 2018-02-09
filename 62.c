70fa40ebc054d9234dce1ae4c39765af18d74dea
AKHILASANTHOSH <30549500+AKHILASANTHOSH@users.noreply.github.com>
2018-02-09 15:47:29 +0530

    62.c

---
+++ 62.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() 
+{
+unsigned a;
+printf("enter the string :\n");
+scanf("%i",&a);
+if(a%10==0|a%10==1)
+{
+	printf("yes");
+	
+}
+else
+{
+	printf("no");
+}
+	return 0;
+}
