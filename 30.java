0e55b5e44de58ef1bdc0c098aa74e3635c3e3e36
AKHILASANTHOSH <30549500+AKHILASANTHOSH@users.noreply.github.com>
2018-02-12 16:44:17 +0530

    30.java

---
+++ 30.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+import java.math.*;
+public Class 30
+{
+public static void main(String s[])
+{
+Calendar c=new Calendar.getInstance();
+System.out.println("current date:"+"current date"+"current month"+"current year");
+System.out.println("current minute:"+"current minurtes"+"current seconds");
+new=Calendar.getInstance();
+new=add(Calendar.Hour);
+Sytem.out.println("time in hours");
+}
+}
