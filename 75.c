b05f682912c113e7c7f0dd40a67156665a27a046
AKHILASANTHOSH <30549500+AKHILASANTHOSH@users.noreply.github.com>
2018-02-21 16:14:28 +0530

    75.c

---
+++ 75.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+char s[10];
+int n;
+printf("enter the string:\n");
+scanf("%s",s);
+n=strlen(s);
+if(n%2==0)
+{
+s[(n/2)-1]='*';
+printf("%s",s);
+}
+else
+{
+s[(n/2)]='*';
+printf("%s",s);
+}
+return 0;
+}
