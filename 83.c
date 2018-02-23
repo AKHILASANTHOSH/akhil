6177b099d552ab402f784481a144ac4914fa2edc
AKHILASANTHOSH <30549500+AKHILASANTHOSH@users.noreply.github.com>
2018-02-23 16:21:53 +0530

    83.c

---
+++ 83.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void) {
+int a,b,c,d,e,f;
+printf("enter values to be multiplied\n");
+scanf("%d %d",&a,&b);
+c=a*b;
+printf("result :%d",c);
+printf("enter values to be divided\n");
+scanf("%d %d",&d,&e);
+f=d%e;
+printf("result :%d",f);
+
+
+
+	return 0;
+}
