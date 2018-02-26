33912140aecbc3b9a3570ac6bcd5beb3ff434c2a
AKHILASANTHOSH <30549500+AKHILASANTHOSH@users.noreply.github.com>
2018-02-26 16:04:57 +0530

    90.c

---
+++ 90.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+char a[10];
+int i,n;
+printf("enter the string:\n");
+scanf("%s",&a);
+n=strlen(a);
+for(i=0;i<n;i++)
+{
+if(a[i]>='0'&&a[i]<='9')
+{
+printf("%c",a[i]);
+}
+}
+}
